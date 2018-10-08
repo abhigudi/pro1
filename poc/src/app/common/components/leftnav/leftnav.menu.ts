3cf059c80ba67bf6324d62db461b1f2919ce032b
siva <sivam1@netapp.com>
2018-10-08 21:24:45 +0530

    left menu mouseleve added

--- poc/src/app/common/components/leftnav/leftnav.menu.ts
+++ poc/src/app/common/components/leftnav/leftnav.menu.ts
@@ -2,8 +2,8 @@
 export const menuItem = [
     { menuIcon:'fas fa-home', menuString: 'Home', link: ''  },
     { menuIcon:'fas fa-chart-bar', menuString: 'Page1', link: 'page1', children:[
-        { menuIcon:'fas fa-child', menuString: 'Child1', link: 'child1' },
-        { menuIcon:'fas fa-child', menuString: 'Child2', link: 'child2', children:[
+        { menuIcon:'fas fa-child', menuString: 'Child1', link: 'page1/child1' },
+        { menuIcon:'fas fa-child', menuString: 'Child2', link: 'page1/child2', children:[
             { menuIcon:'fas fa-child', menuString: 'Sub Child1', link: 'child1' }
         ] },
     ]  },
