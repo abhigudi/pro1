53e2cc61df3b1f6e900e36d577a0c8c81cf11186
Siva <siva@divicog.com>
2018-10-09 20:17:39 +0530

    added ngprime library

--- poc/src/app/common/common.module.ts
+++ poc/src/app/common/common.module.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LeftnavComponent } from './components/leftnav/leftnav.component';
 import { RouterModule } from '@angular/router';
+import {TableModule} from 'primeng/table';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    TableModule
   ],
   declarations: [ HeaderComponent, FooterComponent, LeftnavComponent ],
   exports: [ HeaderComponent, FooterComponent, LeftnavComponent ]
